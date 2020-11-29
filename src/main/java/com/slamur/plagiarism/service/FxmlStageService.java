d1ff107835fec2c66e8a530a77e4dea6da53b97a
Viacheslav Muravev <muravjevslava@gmail.com>
2020-11-29 18:00:00 +0400

    Added maximizing of main window

--- src/main/java/com/slamur/plagiarism/service/FxmlStageService.java
+++ src/main/java/com/slamur/plagiarism/service/FxmlStageService.java
@@ -9,18 +9,9 @@ public class FxmlStageService {
     public static final String DIFF_SCENE_NAME = "diff", DIFF_TITLE = "Сверка решений";
     public static final String CLUSTER_SCENE_NAME = "diff", CLUSTER_TITLE = "Кластеры списавших";
 
-//    public static void showDiffStage() {
-//        Scene scene = createScene(DIFF_SCENE_NAME);
-//        showStage(scene, DIFF_TITLE);
-//    }
-//
-//    public static void showClusterStage() {
-//        Scene scene = createScene(CLUSTER_SCENE_NAME);
-//        showStage(scene, CLUSTER_TITLE);
-//    }
-
     public static void showMainStage() {
-        Scene scene = createScene(MAIN_SCENE_NAME);
-        showStage(scene, MAIN_TITLE);
+        var scene = createScene(MAIN_SCENE_NAME);
+        var stage = showStage(scene, MAIN_TITLE);
+        stage.setMaximized(true);
     }
 }
